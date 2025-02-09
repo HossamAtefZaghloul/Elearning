204ac0001757cb876787c7a63283e76786317fc6
HossamAtefZaghloul <hossamatef.9919@gmail.com>
2025-02-09 08:43:53 +0200

    chor : add User entity and base.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'rahhst',
+      database: process.env.DATABASE_NAME || 'Elearning',
+      synchronize: false,
+      entities: [__dirname + '/database/core/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
     }),
     TestModule,
   ],
