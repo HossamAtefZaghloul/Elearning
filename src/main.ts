e7f4d1841df2c19f3b5b2d4f5bd650edfe3fff1a
HossamAtefZaghloul <hossamatef.9919@gmail.com>
2025-02-15 18:15:33 +0200

    add: Security RATELIMIT for bruteF Attaks + helmet for xss ataks to help with DTO stricted with global-pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +11,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  // Global-*-*-Pipes \\
+  //  \|/-:--:[-Security-]:--:-\|/
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10Min
+      max: 100, // Limit: 100 request per window
+      message: 'Too many requests, please try again later.',
+    }),
+  );
+  app.use(helmet());
+  // Global-Pipes: enables strict ((DTO)) validation and transformation
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3005);
   console.log('Startedddd');
